8e313628fd91e06c68b03433fbde72c1bfc8e4f5
rougho <rohi@rgho.de>
2024-02-13 21:37:41 +0100

    cart done

--- Alimama/store/urls.py
+++ Alimama/store/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
          views.home, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>',
          views.product, name='product_detail'),
-    path('cart', views.cart, name='cart'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart', views.cart_detail, name='cart_detail'),
+
 
 ]
